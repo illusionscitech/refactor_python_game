2a4e77c8dd082f47bfc3b6170334bba1b4c78067
梦幻科技 <836976779@qq.com>
2023-06-21 23:47:46 +0700

    Long Function
    1. Defect type of "Long Function"
    2. What does this problem lead to? Long initialization methods make the code difficult to understand and maintain. When a method contains too much functionality and logic, it increases the difficulty of reading and understanding the code, and also increases the difficulty of modifying and debugging the code. Furthermore, if an error occurs during initialization or a modification is required, it needs to be located and modified in a bulky method, increasing the risk of errors and introducing new bugs.
    What is causing this problem? The reason for this problem is to gather too much functionality and logic in one method. It may be that the initial design did not consider the decomposition function, the lack of awareness of code refactoring, or the continuous increase of functions during the development process without timely refactoring, resulting in lengthy initialization methods.

--- Zombioo-main/button.py
+++ Zombioo-main/button.py
@@ -8,12 +8,15 @@ LEFT_MOUSE_BUTTON = 0
 class Button():
 	#初始化按钮对象，接受参数包括按钮的初始位置(x, y)、按钮的图像image和缩放比例scale。在初始化过程中，将图像按照缩放比例进行缩放，并设置按钮的矩形边界。
 	def __init__(self,x, y, image, scale):
-		width = image.get_width()
-		height = image.get_height()
-		self.image = pygame.transform.scale(image, (int(width * scale), int(height * scale)))
-		self.rect = self.image.get_rect()
+		self.image, self.rect = self._load_image(image, scale)
 		self.rect.topleft = (x, y)
 		self.clicked = False
+
+	def _load_image(self, image, scale):
+		width, height = image.get_size()
+		scaled_image = pygame.transform.scale(image, (int(width * scale), int(height * scale)))
+		rect = scaled_image.get_rect()
+		return scaled_image, rect
 
 	#绘制按钮，并检测鼠标交互操作。该方法接受一个surface参数，表示将按钮绘制在哪个画布上。在绘制过程中，首先获取鼠标的位置，并检查鼠标是否在按钮的矩形边界内。如果鼠标在按钮内，并且鼠标左键被按下且按钮之前没有被点击过，则返回True，表示按钮被点击。在绘制按钮时，使用blit方法将按钮的图像绘制在指定位置上。
 	def draw(self, surface):
